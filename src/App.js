b5b63e85a0c70ef219c1aeb55d5551bd65422f2e
Sutharson-Ma <139798447+Sutharson-Ma@users.noreply.github.com>
2024-05-08 16:19:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NumberInput from './components/NumberInput';
+import ResponseDisplay from './components/ResponseDisplay';
+function App() {
+  return (
+    <div className="App">
+      <NumberInput/>
+      <ResponseDisplay/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NumberInput.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function NumberInput({ onResponse }) {
+  const [numberId, setNumberId] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`http://localhost:9876/numbers/${numberId}`);
+      onResponse(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Number ID:
+        <input type="text" value={numberId} onChange={(e) => setNumberId(e.target.value)} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default NumberInput;
